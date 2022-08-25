e019a0a8e512fead0c9ee169f6bcda3ebb00d353
Bektosh Madaminov <bektosh.madaminov@itechart-group.com>
2022-08-25 22:05:42 +0500

    pushing changes for sync

--- opentelemetry/fib/main.go
+++ opentelemetry/fib/main.go
@@ -9,6 +9,7 @@ import (
 
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/jaeger"
 	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
 	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
@@ -18,21 +19,10 @@ import (
 func main() {
 	l := log.New(os.Stdout, "", 0)
 
-	f, err := os.Create("traces.txt")
+	tp, err := newTracerProvider("http://localhost:14268/api/traces")
 	if err != nil {
 		l.Fatal(err)
 	}
-	defer f.Close()
-
-	exp, err := newExporter(f)
-	if err != nil {
-		l.Fatal(err)
-	}
-
-	tp := trace.NewTracerProvider(
-		trace.WithBatcher(exp),
-		trace.WithResource(newResource()),
-	)
 	defer func() {
 		if err := tp.Shutdown(context.Background()); err != nil {
 			l.Fatal(err)
@@ -60,12 +50,35 @@ func main() {
 	}
 }
 
+func newTracerProvider(url string) (*trace.TracerProvider, error) {
+	exp, err := newJaegerExporter(url)
+	if err != nil {
+		return nil, err
+	}
+
+	res := newResource()
+
+	return trace.NewTracerProvider(
+		trace.WithBatcher(exp),
+		trace.WithResource(res),
+	), nil
+}
+
 func newExporter(w io.Writer) (trace.SpanExporter, error) {
 	return stdouttrace.New(
 		stdouttrace.WithWriter(w),
 		stdouttrace.WithPrettyPrint(),
 		stdouttrace.WithoutTimestamps(),
 	)
+}
+
+func newJaegerExporter(jaegerURL string) (trace.SpanExporter, error) {
+	exp, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(jaegerURL)))
+	if err != nil {
+		return nil, err
+	}
+
+	return exp, nil
 }
 
 func newResource() *resource.Resource {
